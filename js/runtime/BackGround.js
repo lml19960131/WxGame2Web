e5a306af87ce3f0d9cc534ad14d14480d5a8dc6b
lml19960131 <lml19960131@126.com>
2018-02-21 22:49:54 +0800

    Background类

---
+++ js/runtime/BackGround.js
@@ -0,0 +1,13 @@
+//背景
+import {Sprite} from '../base/Sprite.js'
+
+export class BackGround extends Sprite {
+    constructor() {
+        const image = BackGround.getImage('background');
+        super(image,
+            0, 0,
+            image.width, image.height,
+            0, 0,
+            window.innerWidth, window.innerHeight);
+    }
+}
