a56281373c554b6d5d653badb4da75ce762f99c3
lml19960131 <lml19960131@126.com>
2018-02-22 23:30:30 +0800

    Director类文件

--- js/Director.js
+++ js/Director.js
@@ -24,15 +24,70 @@ export class Director {
         this.dataStore.get('pencils').push(new DownPencil(top));
     }
 
-    run() {
-        if(!this.isGameOver){
-            this.dataStore.get("background").draw();
-            const penclis = this.dataStore.get('pencils');
-            if (penclis[0].x + penclis[0].width <= 0 && penclis.length ===4) {
-                penclis.shift();
-                penclis.shift();
+    //让鸟飞起来
+    birdsEvent() {
+        for (let i = 0; i <= 2; i++) {
+            this.dataStore.get('birds').y[i] = this.dataStore.get('birds').birdsY[i];
+        }
+        this.dataStore.get('birds').time = 0;
+    }
+
+    //是否和铅笔撞击
+    static isStrike(bird, pencil) {
+        let s = false;
+        if(bird.top>pencil.bottom || bird.bottom<pencil.top ||
+            bird.right<pencil.left || bird.left>pencil.right){
+            s = true;
+        }
+        return !s;
+    }
+
+    //实现撞击地面和铅笔判断
+    check() {
+        const birds = this.dataStore.get('birds');
+        const land = this.dataStore.get('land');
+        const pencils = this.dataStore.get('pencils');
+        if (birds.birdsY[0] + birds.birdsHeight[0] >= land.y) {
+            console.log('撞击地板');
+            this.isGameOver = true;
+        }
+
+        //小鸟的边框
+        const birdsBorder = {
+            top: birds.y[0],
+            right: birds.birdsX[0] + birds.birdsWidth[0],
+            bottom: birds.y[0] + birds.birdsHeight[0],
+            left: birds.birdsX[0],
+        };
+        //铅笔的边框
+        const length = pencils.length;
+        for (let i = 0; i < length; i++) {
+            const pencil = pencils[i];
+            const pencilBorder = {
+                top: pencil.y,
+                bottom: pencil.y + pencil.height,
+                left: pencil.x,
+                right: pencil.x + pencil.width,
+            };
+
+            if(Director.isStrike(birdsBorder, pencilBorder)){
+                console.log('撞击到铅笔');
+                this.isGameOver = true;
+                return;
             }
-            if(penclis[0].x<=(window.innerWidth-penclis[0].width)/2&& penclis.length ===2) {
+        }
+    }
+
+    run() {
+        this.check();
+        if (!this.isGameOver) {
+            this.dataStore.get("background").draw();
+            const pencils = this.dataStore.get('pencils');
+            if (pencils[0].x + pencils[0].width <= 0 && pencils.length === 4) {
+                pencils.shift();
+                pencils.shift();
+            }
+            if (pencils[0].x <= (window.innerWidth - pencils[0].width) / 2 && pencils.length === 2) {
                 this.createPencil();
             }
             this.dataStore.get('pencils').forEach(function (value) {
@@ -43,6 +98,7 @@ export class Director {
             let timer = requestAnimationFrame(() => this.run()); //用于动画，刷新率有浏览器决定，性能好
             this.dataStore.put('timer', timer);
         } else {
+            console.log('GameOver');
             cancelAnimationFrame(this.dataStore.get('timer'));
             this.dataStore.destory();
         }
