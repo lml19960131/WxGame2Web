33e5d2b3fa8d523bf7232691c7ec82464606b9e4
lml19960131 <lml19960131@126.com>
2018-02-23 22:40:50 +0800

    障碍物创建

--- js/runtime/Pencil.js
+++ js/runtime/Pencil.js
@@ -2,19 +2,21 @@
  * Created by 62354 on 2018/2/21.
  */
 import {Sprite} from '../base/Sprite.js'
-import {Director} from '../Director.js'
+import {DataStore} from '../base/DataStore.js'
+
 export class Pencil extends Sprite {
     constructor(image, top) {
         super(image,
             0, 0,
             image.width, image.height,
-            window.innerWidth, 0,
+            DataStore.getInstance().canvas.width, 0,
             image.width, image.height);
         this.top = top; //this不能出现在super上面
+        this.moveSpeed = 2;
     }
 
     draw() {
-        this.x = this.x - Director.getInstance().moveSpeed;
+        this.x = this.x - this.moveSpeed;
         super.draw(this.img,
             0, 0,
             this.img.width, this.img.height,
