19a19856dc3f3da3590f4dac311e8fa098d64587
lml19960131 <lml19960131@126.com>
2018-02-23 22:37:48 +0800

    下部障碍物适配

--- js/runtime/DownPencil.js
+++ js/runtime/DownPencil.js
@@ -1,6 +1,7 @@
 //下半部分铅笔
 import {Pencil} from './Pencil.js'
 import {Sprite} from '../base/Sprite.js'
+import {DataStore} from '../base/DataStore.js'
 
 export class DownPencil extends Pencil {
     constructor(top) {
@@ -9,7 +10,7 @@ export class DownPencil extends Pencil {
     }
 
     draw() {
-        let gap = window.innerHeight / 5;
+        let gap = DataStore.getInstance().canvas.height / 5;
         this.y = this.top + gap;
         super.draw();
     }
