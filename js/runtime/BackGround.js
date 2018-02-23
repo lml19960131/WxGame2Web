a4432907a6f57a9208b01b32a5ec0e4ed191ce47
lml19960131 <lml19960131@126.com>
2018-02-23 22:36:45 +0800

    背景类及适配

--- js/runtime/BackGround.js
+++ js/runtime/BackGround.js
@@ -1,5 +1,6 @@
 //背景
 import {Sprite} from '../base/Sprite.js'
+import {DataStore} from '../base/DataStore.js'
 
 export class BackGround extends Sprite {
     constructor() {
@@ -8,6 +9,6 @@ export class BackGround extends Sprite {
             0, 0,
             image.width, image.height,
             0, 0,
-            window.innerWidth, window.innerHeight);
+            DataStore.getInstance().canvas.width, DataStore.getInstance().canvas.height);
     }
 }
