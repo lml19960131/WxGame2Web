97d5ca6b0c476874688bd832959992fc0240552b
lml19960131 <lml19960131@126.com>
2018-02-23 22:36:08 +0800

    开始按钮及适配

--- js/player/StartButton.js
+++ js/player/StartButton.js
@@ -1,5 +1,6 @@
 //开始按钮
 import {Sprite} from '../base/Sprite.js'
+import {DataStore} from '../base/DataStore.js'
 
 export class StartButton extends Sprite {
     constructor() {
@@ -8,7 +9,7 @@ export class StartButton extends Sprite {
             image,
             0, 0,
             image.width, image.height,
-            (window.innerWidth - image.width) / 2, (window.innerHeight - image.height) / 2.5,
+            (DataStore.getInstance().canvas.width - image.width) / 2, (DataStore.getInstance().canvas.height - image.height) / 2.5,
             image.width, image.height
         );
     }
