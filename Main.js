7d48b5d61ccb259bdb305bc2d0c9d6c2d5ccf784
lml19960131 <lml19960131@126.com>
2018-02-23 22:42:23 +0800

    游戏入口

--- Main.js
+++ Main.js
@@ -5,24 +5,43 @@ import {DataStore} from './js/base/DataStore.js'
 import {Director} from './js/Director.js'
 import {Land} from './js/runtime/Land.js'
 import {Birds} from './js/player/Birds.js'
+import {StartButton} from './js/player/StartButton.js'
+import {Score} from './js/player/Score.js'
+import {ApiExamples} from './js/ApiExample.js'
 
 export class Main {
     constructor() {
-        this.canvas = document.getElementById('game-canvas');
+        this.canvas = wx.createCanvas();
         this.ctx = this.canvas.getContext('2d');
         this.dataStore = DataStore.getInstance();
         this.director = Director.getInstance();
         const loader = ResourceLoader.create();
         loader.onLoaded(map => this.onResourceFirstLoaded(map));
-        let image = new Image();
-        image.src = './res/background.png';
+        // let image = wx.createCanvas();
+        // image.src = './res/background.png';
+    }
+
+    //创建bgm
+    createBackgroundMusic() {
+        const bgm = wx.createInnerAudioContext();
+        bgm.autoplay = true;
+        bgm.loop = true;
+        bgm.src = 'bgm/bgm.mp3';
     }
 
     onResourceFirstLoaded(map) {
+        this.dataStore.canvas = this.canvas;
         this.dataStore.ctx = this.ctx;
         this.dataStore.res = map;
+        // this.createBackgroundMusic();
+        const example = new ApiExamples();
+        // example.getUserInfo(); 获取用户信息
+        // example.login(); 获取登陆时的信息如code用于转第三方或者支付
+        // example.getSetting(); 获取个人配置信息
+        // example.httpExample(); 获取HTTP请求数据
         this.init();
     }
+
 
     init() {
         this.director.isGameOver = false;
@@ -31,21 +50,31 @@ export class Main {
             .put('pencils', [])
             .put('background', BackGround) //ES6中的类是以function存在的
             .put('land', Land)
-            .put('birds', Birds);
+            .put('birds', Birds)
+            .put('score', Score)
+            .put('startButton', StartButton);
         this.registerEvent();
         this.director.createPencil();
         this.director.run()
     }
 
     registerEvent() {
-        this.canvas.addEventListener('touchstart', e => {
-            e.preventDefault();
+        // this.canvas.addEventListener('touchstart', e => {
+        //     e.preventDefault();
+        //     if (this.director.isGameOver) {
+        //         console.log('游戏开始');
+        //         this.init();
+        //     } else {
+        //         this.director.birdsEvent();
+        //     }
+        // });
+        wx.onTouchStart(() => {
             if (this.director.isGameOver) {
                 console.log('游戏开始');
                 this.init();
             } else {
                 this.director.birdsEvent();
             }
-        });
+        })
     }
 }
