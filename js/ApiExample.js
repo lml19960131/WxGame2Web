1d45098f4db75decc67d1c193d0dcd7cca389fdd
lml19960131 <lml19960131@126.com>
2018-02-24 22:42:59 +0800

    API使用练习

--- js/ApiExample.js
+++ js/ApiExample.js
@@ -4,12 +4,13 @@
 export class ApiExamples {
     getUserInfo() {
         const params = {
-            success: function(res) {
+            success: function (res) {
                 console.log(res);
             }
         };
         wx.getUserInfo(params)
     }
+
     login() {
         wx.login({
             success: function (res) {
@@ -17,6 +18,7 @@ export class ApiExamples {
             }
         })
     }
+
     getSetting() {
         wx.getSetting({
             success: function (res) {
@@ -24,13 +26,38 @@ export class ApiExamples {
             }
         })
     }
+
     httpExample() {
         wx.request({
             url: 'http://127.0.0.1:8181',//内部的回环地址8181端口号
             method: 'POST',
             data: 'myData',
-            success:function (response) {
+            success: function (response) {
                 console.log(response); //这里可以根据服务器的返回值来作相关操作
+            }
+        })
+    };
+
+    socketExample() {
+        wx.connectSocket({
+            url: 'ws://127.0.0.1:8888',
+            success: function () {
+                console.log('客户端连接成功');
+            }
+        });
+        //发送数据必须使用wx.onSocketOpen()
+        wx.onSocketOpen(function () {
+            wx.sendSocketMessage({
+                data: '这是来自客户端的消息'
+            })
+        })
+    };
+
+    download() {
+        wx.downloadFile({
+            url: '资源位置',
+            success: function (temp) {
+                console.log(JSON.stringify(temp));
             }
         })
     }
